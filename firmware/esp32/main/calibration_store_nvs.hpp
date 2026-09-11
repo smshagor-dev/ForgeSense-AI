1c36e89f6e6a7dddae42c2e2c1bc6793eabcd6fa
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-09-11 22:39:53 +0300

    fix: track calibration NVS store readiness

--- firmware/esp32/main/calibration_store_nvs.hpp
+++ firmware/esp32/main/calibration_store_nvs.hpp
@@ -24,16 +24,19 @@ public:
     std::uint32_t installed_floor() const { return installed_floor_; }
     bool has_active() const { return has_active_; }
     std::uint8_t active_slot() const { return active_slot_; }
+    bool ready() const { return ready_; }
 
 private:
     bool read_slot(std::uint8_t slot, std::array<std::uint8_t, sensing::kCalibrationBlobSize>& out, bool& present) const;
     bool write_metadata(std::uint8_t active_slot, std::uint32_t floor);
+    void reset_failed_open();
 
     nvs_handle_t handle_{0};
     std::uint32_t installed_floor_{0};
     std::uint8_t active_slot_{0};
     bool has_active_{false};
     bool opened_{false};
+    bool ready_{false};
     sensing::CalibrationRecord active_record_{};
 };
 
